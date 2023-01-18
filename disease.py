0320cf82502fc17075aeb66c8a4808db3679f455
RIYA BHATT <riyabhatt548@gmail.com>
2023-01-18 19:33:25 +0530

    Create disease.py

---
+++ disease.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+heart_data = pd.read_csv('/content/data.csv')
+heart_data.head()
+heart_data.describe()
