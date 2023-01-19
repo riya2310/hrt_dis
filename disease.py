f3a8cb266989ed942b802f8acb31e4148c235ba8
RIYA BHATT <riyabhatt548@gmail.com>
2023-01-19 22:53:54 +0530

    Update disease.py

--- disease.py
+++ disease.py
@@ -7,3 +7,8 @@ from sklearn.metrics import accuracy_score
 heart_data = pd.read_csv('/content/data.csv')
 heart_data.head()
 heart_data.describe()
+# checking the distribution of Target Variable
+heart_data['target'].value_counts()
+X = heart_data.drop(columns='target', axis=1)
+Y = heart_data['target']
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=2)
