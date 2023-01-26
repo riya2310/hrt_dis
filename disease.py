786c5c4131b652aaee1f3453cc8ffdcd4d2e8bd4
RIYA BHATT <riyabhatt548@gmail.com>
2023-01-26 21:41:41 +0530

    Update disease.py

--- disease.py
+++ disease.py
@@ -16,4 +16,26 @@ print(X.shape, X_train.shape, X_test.shape)
 model = LogisticRegression()
 # training the LogisticRegression model with Training data
 model.fit(X_train, Y_train)
+# accuracy on training data
+X_train_prediction = model.predict(X_train)
+training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
+# accuracy on test data
+X_test_prediction = model.predict(X_test)
+test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
+print('Accuracy on Test data : ', test_data_accuracy)
 
+input_data = (62,0,0,140,268,0,0,160,0,3.6,0,2,2)
+
+# change the input data to a numpy array
+input_data_as_numpy_array= np.asarray(input_data)
+
+# reshape the numpy array as we are predicting for only on instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+prediction = model.predict(input_data_reshaped)
+print(prediction)
+
+if (prediction[0]== 0):
+  print('The Person does not have a Heart Disease')
+else:
+  print('The Person has Heart Disease')
